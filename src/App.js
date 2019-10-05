5784772ddf463554a9049425e411d4a2edbec068
Ellen Nitchals <ellen.nitchals@gmail.com>
2019-10-05 13:55:42 -0700

    button is clicky

--- src/App.js
+++ src/App.js
@@ -6,13 +6,53 @@ class App extends React.Component {
   constructor(){
     super()
     this.state = {
-      showerIsOn: false
+      showerIsOn: false,
+      currentShowerId: null,
+      showerStartedAt: null,
+      showerEndedAt: null,
+      durationMessage: '',
     }
   }
   toggleShower = () => {
-    this.setState({showerIsOn: !this.state.showerIsOn})
-    // make a call to the server to create a record
+    this.state.showerIsOn ? this.startShower() : this.endShower();
+    this.setState({showerIsOn: !this.state.showerIsOn});
   }
+
+  startShower = () => {
+    // set this.state.showerStartedAt to the current timestamp
+    this.setState({showerStartedAt: Date.now()})
+    fetch('https://shower-power.herokuapp.com/api/v1/showers',{
+      method: 'POST',
+      body: JSON.stringify({}),
+      headers: {"Content-Type": "application/json"}
+    })
+    .then(res => {
+      // this.setState({currentShowerId: res.body._id})
+      console.log(this.state.currentShowerId)
+      console.log(JSON.stringify(res.body))
+    }).catch(err => console.log(err))
+  }
+
+  endShower = () => {
+    // calculate a duration and add to the record
+    this.setState({showerEndedAt: Date.now()})
+    const duration = Date.now() - this.state.showerStartedAt;
+    fetch(`https://shower-power.herokuapp.com/api/v1/showers/${this.state.currentShowerId}`, {
+      method: 'PATCH',
+      body: JSON.stringify({duration})
+    })
+  }
+
+  calculateDuration = () => {
+      let duration = this.state.showerEndedAt - this.state.showerStartedAt;
+     const hoursDifference = Math.floor(duration/1000/60/60);
+      duration -= hoursDifference*1000*60*60
+      const minutesDifference = Math.floor(duration/1000/60);
+      duration -= minutesDifference*1000*60
+      const secondsDifference = Math.floor(duration/1000);
+      this.setState({durationMessage: `${hoursDifference} hours ${minutesDifference} minutes ${secondsDifference} seconds`})
+  }
+
   render() {
     return (
       <div className="App">
