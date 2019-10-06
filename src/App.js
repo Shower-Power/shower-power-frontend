e6a2bdd88e85ef071e97cc2f3694b2ebb858bb56
Ellen Nitchals <ellen.nitchals@gmail.com>
2019-10-06 09:23:46 -0700

    aggregating and displaying some data now

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Button from './Button';
-// import Data from './Data';
+import Data from './Data';
 import './App.css';
 
 const axios = require('axios');
@@ -22,12 +22,18 @@ class App extends React.Component {
       showerStartedAt: null,
       durationMessage: '',
       duration: null,
-      timer: null
+      timer: null,
+      avgs: {
+        length: null,
+        maxT: null,
+        maxH: null,
+        incH: null,
+        incT: null
+      }
     }
   }
 
   toggleShower = () => {
-    console.log(this.state.showerIsOn)
     this.state.showerIsOn ? this.endShower() : this.startShower();
     const showerIsOn = !this.state.showerIsOn
     this.setState({showerIsOn});
@@ -46,27 +52,78 @@ class App extends React.Component {
     const duration = Date.now() - this.state.showerStartedAt;
     this.calculateDuration()
     const {initialTemperature, initialHumidity, peakHumidity, peakTemperature} = this.state;
-    axios.patch(`https://shower-power.herokuapp.com/api/v1/showers/${this.state.currentShowerId}`, {duration, tempBefore: initialTemperature, humidityBefore: initialHumidity, tempPeak: peakTemperature, humidityPeak: peakHumidity})
+    const currentShower = {
+      duration,
+      tempBefore: initialTemperature,
+      humidityBefore: initialHumidity,
+      tempPeak: peakTemperature,
+      humidityPeak: peakHumidity
+    }
+    this.getAllShowerData(currentShower);
+    axios.patch(`https://shower-power.herokuapp.com/api/v1/showers/${this.state.currentShowerId}`, {      duration,
+    tempBefore: initialTemperature,
+    humidityBefore: initialHumidity,
+    tempPeak: peakTemperature,
+    humidityPeak: peakHumidity})
     clearInterval(this.state.timer)
   }
 
-  getAllShowerData = () => {
+  getAllShowerData = (currentShower) => {
+    console.log("current", currentShower)
     axios.get('https://shower-power.herokuapp.com/api/v1/showers/').then(res => {
-      console.log(res.data)
+      const data = res.data.concat(currentShower)
+      this.calculateAverages(data)
     })
+  }
+
+  calculateAverages = (data) => {
+    const length = this.calcAvg(this.getValues(data, "duration"));
+    const maxT = this.calcAvg(this.getValues(data, "tempPeak"));
+    const maxH = this.calcAvg(this.getValues(data, "humidityPeak"));
+    const incT = this.calcAvg(this.getCalculatedValues(data, "humidityPeak", "humidityBefore"));
+    const incH = this.calcAvg(this.getCalculatedValues(data, "tempPeak", "tempBefore"));
+
+    const avgs = {
+      length,
+      maxT,
+      maxH,
+      incT,
+      incH
+    }
+    this.setState({avgs})
+  }
+
+  getValues = (data, dataType) => {
+    const values = [];
+    data.forEach(shower => {if (shower[dataType]) values.push(shower[dataType])})
+    return values;
+  }
+
+  getCalculatedValues = (data, dataType1, dataType2) => {
+    const values = [];
+    data.forEach(shower => {if (shower[dataType1] && shower[dataType2]) values.push(shower[dataType1] - shower[dataType2])})
+    return values;
+  }
+
+  calcAvg = (data) => {
+    const total = data.reduce((acc, cur) => acc + cur);
+    return total/data.length
   }
 
   calculateDuration = () => {
       let duration = Date.now() - this.state.showerStartedAt;
-      this.setState({duration})
-      let durationMessage;
-        const hoursDifference = Math.floor(duration/1000/60/60);
-        duration -= hoursDifference*1000*60*60
-        const minutesDifference = Math.floor(duration/1000/60);
-        duration -= minutesDifference*1000*60
-        const secondsDifference = Math.floor(duration/1000);
-        durationMessage = `${minutesDifference}m ${secondsDifference}s`
-      this.setState({durationMessage})
+      this.setState({duration, durationMessage: this.prettyDuration(duration)})
+  }
+
+  prettyDuration = (duration) => {
+    let durationMessage;
+    const hoursDifference = Math.floor(duration/1000/60/60);
+    duration -= hoursDifference*1000*60*60
+    const minutesDifference = Math.floor(duration/1000/60);
+    duration -= minutesDifference*1000*60
+    const secondsDifference = Math.floor(duration/1000);
+    durationMessage = `${minutesDifference}m ${secondsDifference}s`
+    return durationMessage;
   }
 
   getInitialData = () => {
@@ -78,7 +135,6 @@ class App extends React.Component {
   }
 
   checkCurrentData = () => {
-    console.log("checking data")
     this.getCurrentData();
     window.setTimeout(() => {
       if (this.state.currentTemperature > this.state.peakTemperature) this.setState({peakTemperature: this.state.currentTemperature})
@@ -94,20 +150,27 @@ class App extends React.Component {
   }
 
   render() {
-    console.log(this.state);
     const data = {
-      length: this.state.duration,
-      averageLength: 0, //calc
-      diffFromAverage: 0, //calc
+      length: this.state.durationMessage,
+      averageLength: this.prettyDuration(this.state.avgs.length),
+      diffFromAverage: this.prettyDuration(Math.abs(this.state.avgs.length - this.state.duration)),
       startH: this.state.initialHumidity,
       startT: this.state.initialTemperature,
       peakH: this.state.peakHumidity,
-      peakT: this.state.peakTemperature
+      peakT: this.state.peakTemperature,
+      avgPeakH: this.state.avgs.peakT,
+      avgPeakT: this.state.avgs.peakH,
+      diffH: this.state.peakHumidity - this.state.initialHumidity,
+      diffT: this.state.peakTemperature - this.startShower.initialTemperature,
+      avgDiffH: this.state.avgs.incH,
+      avgDiffT: this.state.avgs.incT
     }
     return (
       <div className="App">
+        <div className="app-title">SHWR PWR!</div>
+        <div className="app-headline">keep it short!</div>
         <Button type={!this.state.showerIsOn ? 'turn-shower-on' : 'turn-shower-off' } onClick={this.toggleShower}/>
-        {/* <Data data={data}/> */}
+        {!this.state.showerIsOn && this.state.duration ? <Data data={data}/> : null}
       </div>
     )
   }
--- src/Data.js
+++ src/Data.js
@@ -2,31 +2,53 @@ import React from 'react';
 import './App.css';
 
 class Data extends React.Component {
+    constructor(props){
+        super(props)
+    }
     render(){
         // length of shower
         // average shower time, how much longer/shorter this time
         // start, peak, delta humidity
         // start, peak, delta temp
+        const {length, averageLength, diffFromAverage} = this.props.data;
         return (
             <div className="Data">
                 <div className="data-row">
-                    <div className="data-point">
-                        <div className="data-point-large">{this.state.length}</div>
-                        <div className="data-point-label">Length of shower</div>
-                    </div>
-                    <div className="data-point">
-                        <div className="data-point-large">{this.state.diffFromAverage}</div>
-                        <div className="data-point-label">{`${length > averageLength ? 'Longer':'Shorter'} than average`}</div>
-                    </div>
+                    <DataPoint data={length} label="length of shower" type="large"/>
+                    <DataPoint data={averageLength} label="avg shower time" type="large"/>
+                    <DataPoint length={diffFromAverage} label = {`${length > averageLength ? 'Longer':'Shorter'} than average`} type="large"/>
                 </div>
-                <div className="data-row"></div>
-                <div className="data-row"></div>
-                <div className="data-row"></div>
-                <div className="data-row"></div>
-                <div className="data-row"></div>
+                {/* <div className="data-row">
+                    <DataPoint data={peakT} label={} type="large"/>
+                    <DataPoint data={avgPeakT} label={} type="small"/>
+                    <DataPoint data={diffT} label={} type="large"/>
+                    <DataPoint data={avgDiffT} label={} type="small"/>
+                </div>
+                <div className="data-row">
+                    <DataPoint data={peakH} label={} type="large"/>
+                    <DataPoint data={avgPeakH} label={} type="small"/>
+                    <DataPoint data={diffH} label={} type="large"/>
+                    <DataPoint data={avgDiffH} label={} type="small"/>
+                </div> */}
+
             </div>
         );
     }
 }
 
 export default Data;
+
+class DataPoint extends React.Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        const {data, label, type} = this.props;
+        return(
+            <div className="data-point">
+                <div className={`data-point-${type}`}>{data}</div>
+                <div className="data-point-label">{label}</div>
+            </div>
+        )
+    }
+}
